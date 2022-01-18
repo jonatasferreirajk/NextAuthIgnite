04893b673e6ce4fbdf69e1f95ce0129d003d2c8a
Jonatas Ferreira <silvajonatas2@gmail.com>
2022-01-18 12:51:40 -0300

    Salvando tokens nos cookies

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
 import Router from "next/router";
+import { setCookie } from "nookies";
 import { createContext, ReactNode, useState } from "react";
 import { api } from "../services/api";
 
@@ -21,7 +22,7 @@ type User = {
   email: string;
   permissions: string[];
   roles: string[];
-}
+};
 
 export const AuthContext = createContext({} as AuthContextData);
 
@@ -36,16 +37,25 @@ export function AuthProvider({ children }: AuthProviderProps) {
         password,
       });
 
-      const {permissions, roles} = response.data;
+      const { permissions, roles, token, refreshToken } = response.data;
+
+      setCookie(undefined, "nextauth.token", token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: "/",
+      });
+
+      setCookie(undefined, "nextauth.refreshToken", refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: "/",
+      });
 
       setUser({
         email,
         permissions,
-        roles
+        roles,
       });
 
-       Router.push('/dashboard');
-
+      Router.push("/dashboard");
     } catch (err) {
       console.log(err);
     }
